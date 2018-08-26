4c2b33b27c607a62333cdc8925ef51404217502b
Richard Garner <richardgarner@mac.com>
2018-08-25 20:30:06 -0500

    yellow block is moving with typing

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 $("#keyboard-upper-container").hide();
 let sentences = ['ten', 'Too', 'oat', 'itant', 'nee'];
 let a = 0;
-$("<p>" + sentences[a] + "</p>").appendTo("#sentence")
+$(`<p> ${sentences[a]} </p>`).appendTo("#sentence")
 // a++;
 // console.log(a)
 // $("<p>" + sentences[a] + "</p>").appendTo("#sentence")
+let yellowBox = $("#yellow-block")
 
 $(document).keydown(function (d) {
     if (d.keyCode == 16) {
@@ -20,17 +21,18 @@ $(document).keydown(function (d) {
     });
 });
 $(document).keypress(function (e) {
-    console.log("Ascii : " + e.which);
+    console.log(`Ascii : ${e.which}`);
 });
 $(document).keypress(function (k) {
     let key = k.which
-    $("#" + key).css("background-color", "yellow")
+    $(`#${key}`).css("background-color", "yellow")
     $(document).keyup(function () {
-        $("#" + key).css("background-color", "#F5F5F5")
+        $(`#${key}`).css("background-color", "#F5F5F5")
     });
 });
 let index = 0;
-let displayChar = sentences[a].charCodeAt(index)
+let displayChar = sentences[a].charCodeAt(index);
+let yellowShift = 0;
 console.log(displayChar)
 $(document).keypress(function(e){
     let char = sentences[a].charCodeAt(index)
@@ -39,26 +41,33 @@ $(document).keypress(function(e){
         index++
         displayChar = sentences[a].charCodeAt(index)
         console.log(displayChar)
+        yellowShift = yellowShift + 10;
+        $(yellowBox).css("margin-left", yellowShift + "px")
+        console.log(yellowShift)
         if (isNaN(displayChar)) {
             $("p").remove();
             index = 0
             a++
+            $(yellowBox).css("margin-left", "0px")
+            yellowShift = 0
+            console.log(yellowShift)
             if (a == 5) {
                 window.alert("you did it!")
             } else {
-            $("<p>" + sentences[a] + "</p>").appendTo("#sentence")
+            $(`<p> ${sentences[a]} </p>`).appendTo("#sentence")
             displayChar = sentences[a].charCodeAt(index)
-            console.log("displayChar changed to " + displayChar,)
-            console.log("index changed to " + index)
-            console.log("a changed to " + a)}
+            console.log(`displayChar changed to ${displayChar}`)
+            console.log(`index changed to ${index}`)
+            console.log(`a changed to ${a}`)}
         }
     } else {
         console.log("WRONG KEY, DOOFUS!!")
-        console.log("displayChar is " + displayChar)
-        console.log("index is " + index)
-        console.log("a is " + a)
+        console.log(`displayChar is + ${displayChar}`)
+        console.log(`index is ${index}`)
+        console.log(`a is + ${a}`)
     }
 })
+
 
 //$("").click(function () {
     //$("#rand").css("color", "red");
